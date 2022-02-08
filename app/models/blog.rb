4ce1682d81c31e215fe8cdcb30b7748d6ad398d3
hamza12233 <hafizhamzasaleem4@gmail.com>
2022-02-08 10:02:46 +0500

    es search added

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,10 @@
 class Blog < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  index_name Rails.application.class.parent_name.underscore
+  document_type self.name.downcase
+
   belongs_to :user
 
   ATTRIBUTES_WHITELIST = [
@@ -10,6 +16,28 @@ class Blog < ApplicationRecord
   validates :title, presence: true, length: {minimum:3}
 
   scope :search_blog, ->(blog) { where("title LIKE ?", "%"+blog+"%") }
+
+  settings index: { number_of_shards: 1 } do
+    mapping dynamic: false do
+      indexes :title, analyzer: 'english'
+    end
+  end
+
+  def as_indexed_json(options = nil)
+    self.as_json( only: [ :title ] )
+  end
+
+  def self.search(query)
+    __elasticsearch__.search(
+    {
+      query: {
+         multi_match: {
+           query: query,
+           fields: ['title^5']
+         }
+       },
+    })
+  end
 
   def as_json
     super({
